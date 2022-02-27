c12b5a47032561771e14ad9fced2f73ede468a43
Jardiano Rei Delas <jardiano.almeida@gmail.com>
2022-02-27 12:50:14 -0300

    Constraint de Largura e Altura | Alinhamento na Vertical e Horizontal

--- Auto-Layout/Auto-Layout/ViewController.swift
+++ Auto-Layout/Auto-Layout/ViewController.swift
@@ -17,10 +17,16 @@ class ViewController: UIViewController {
 
         redView.backgroundColor = .red
         redView.translatesAutoresizingMaskIntoConstraints = false
-        redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
-        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
-        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
-        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+      //  redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+     //   redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
+    //    redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+   //     redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+        
+        
+        redView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        redView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
 
     }
 
