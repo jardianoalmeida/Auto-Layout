23fd1e11aad2eb04215db544cfcc39378093b57f
Jardiano Rei Delas <jardiano.almeida@gmail.com>
2022-02-27 13:04:36 -0300

    UIStackView - alinhando itens na vertical e horizontal

--- Auto-Layout/Auto-Layout/ViewController.swift
+++ Auto-Layout/Auto-Layout/ViewController.swift
@@ -13,22 +13,40 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let redView = UIView();
-        view.addSubview(redView)
-
         redView.backgroundColor = .red
-        redView.translatesAutoresizingMaskIntoConstraints = false
-      //  redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
-     //   redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
-    //    redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
-   //     redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
-        
-        
-        redView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        redView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
 
+        let blueView = UIView();
+        blueView.backgroundColor = .blue
+        
+        let yellowView = UIView();
+        yellowView.backgroundColor = .yellow
+//
+//        view.addSubview(redView)
+//        view.addSubview(blueView)
+
+//        redView.translatesAutoresizingMaskIntoConstraints = false
+//        redView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+//        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+//        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+//        redView.widthAnchor.constraint(equalToConstant: view.bounds.width / 2).isActive = true
+//
+//        blueView.translatesAutoresizingMaskIntoConstraints = false
+//        blueView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+//        blueView.trailingAnchor.constraint(equalTo: redView.trailingAnchor).isActive = true
+//        blueView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+//        blueView.widthAnchor.constraint(equalToConstant: view.bounds.width / 2).isActive = true
+        
+        let horizontalStackView = UIStackView(arrangedSubviews: [redView, blueView])
+        horizontalStackView.distribution = .fillEqually
+        let stackView: UIStackView = UIStackView(arrangedSubviews: [horizontalStackView, yellowView])
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
-
-
 }
